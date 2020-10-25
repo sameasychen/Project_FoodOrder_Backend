a27726e6d56dfc67c00a3bef414f1e5817a46a18
sameasychen <sam.hs.chen@gmail.com>
2020-10-24 23:39:45 -0400

    Use Mongoose to connect to MongoDB database

--- app.js
+++ app.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://foodorder_User:' + process.env.MONGO_ATLAS_PW +'@cluster0.j4e4i.mongodb.net/' + process.env.MONGO_ATLAS_NAME +'?retryWrites=true&w=majority',{
+    useMongoClient:true
+})
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
